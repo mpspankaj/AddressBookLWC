d834553bf0ccd13fcb7a2082abd8368ad88fc990
PANKAJ-SHARMA1\Pankaj <pankaj.sharma@financialforcecom>
2020-12-12 16:43:14 +0530

    Added Exp List

--- force-app/main/default/lwc/addressBook/addressBook.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-
-
-export default class AddressBook extends LightningElement {
-
-}
---
+++ force-app/main/default/lwc/expenseList/__tests__/expenseList.test.js
@@ -0,0 +1,6 @@
+describe('Add 2 number',()=>{
+    test('added',()=>{
+        const sum = 2+3;
+        expect(sum).toBe(5)
+    })
+})
