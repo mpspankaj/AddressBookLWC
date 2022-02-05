94d39619e6ffbccbd7c903a854c3f8094b755d0d
Pankaj Sharma <mpspankaj@gmail.com>
2022-02-05 06:31:56 +0530

    Overlay Library:Dynamic modal popup and popovers

---
+++ force-app/main/default/aura/overlayWithAura/overlayWithAuraController.js
@@ -0,0 +1,29 @@
+({
+  handleShowModalFooter: function (component, event, helper) {
+    var modalBody;
+    var modalFooter;
+    console.log("handleShowModalFooter");
+    $A.createComponents(
+      [
+        ["c:modalContent", {}],
+        ["c:modalFooter", {}]
+      ],
+      function (components, status) {
+        if (status === "SUCCESS") {
+          modalBody = components[0];
+          modalFooter = components[1];
+          component.find("overlayLib").showCustomModal({
+            header: "Application Confirmation",
+            body: modalBody,
+            footer: modalFooter,
+            showCloseButton: true,
+            cssClass: "my-modal,my-custom-class,my-other-class,cModalOpener",
+            closeCallback: function () {
+              alert("You closed the alert!");
+            }
+          });
+        }
+      }
+    );
+  }
+});
